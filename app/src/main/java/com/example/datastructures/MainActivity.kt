07a61c27fcce410e20eac04ebc52c46fa2dadb04
abedur <abedur_chowdhury@outlook.com>
2024-04-01 17:40:28 -0400

    first commit

---
+++ app/src/main/java/com/example/datastructures/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.datastructures
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.datastructures.databinding.ActivityMainBinding
+import com.example.datastructures.ui.theme.DataStructuresTheme
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        setContent {
+//            DataStructuresTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DataStructuresTheme {
+        Greeting("Android")
+    }
+}
